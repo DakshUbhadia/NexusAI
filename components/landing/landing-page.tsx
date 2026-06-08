d1da5fc3236572239b8c057163e6a362bee34175
Daksh Ubhadia <202301014@dau.ac.in>
2026-06-08 08:54:47 +0530

    added Feature section in landing page

--- components/landing/landing-page.tsx
+++ components/landing/landing-page.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/landing/layout/Footer";
 import Navbar from "@/components/landing/layout/Navbar";
+import FeaturesSection from "./sections/FeaturesSection";
 import HeroSection from "./sections/HeroSection";
 
 export function LandingPage() {
@@ -8,6 +9,7 @@ export function LandingPage() {
       <Navbar />
       <main>
         <HeroSection />
+        <FeaturesSection />
       </main>
       <Footer />
     </>
